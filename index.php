ffd20d7e74ab4d704bfe61922cd283a14bd4c495
Jonas Hellwig <info@kulturbanause.de>
2016-05-05 17:40:46 +0200

    Basis-Dateien hochgeladen
    index.php und Logo

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Seite im Aufbau</title>
+
+<style type="text/css"> 
+			html, body {
+				width:100%;
+				height:100%;
+				padding:0;
+				margin:0;
+				font-family:Verdana, Arial, Helvetica, sans-serif;
+				font-size:11px;
+				line-height:18px;
+				color:#333;
+			}
+ 
+			div#centerhelper {
+				width:1px;
+				height:50%;
+				margin-bottom:-250px; /* half of container's height */
+				float:left;
+			}
+			
+			div#container {
+				width:600px;
+				height:300px;
+				margin:0 auto;
+				clear:left;
+			}
+			
+			div#content {
+				text-align:center;
+			}
+			
+			a {
+				color:#333;
+			}
+		</style> 
+
+</head>
+
+<body>
+
+<div id="centerhelper"></div>
+<div id="container">
+<div id="content">
+<img src="under-cunstruction-logo.png" width="600" height="120" alt="Logo" />
+<p><strong><?php $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+echo $url;
+
+?></strong></p><p>
+
+Diese Website wird aktuell von <a href="http://www.kulturbanause.de">kulturbanause</a> entwickelt. </p></div>
+</div>
+</body>
+</html>
